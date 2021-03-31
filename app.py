788e7cdada7347c1755c86b304259f34d1b60764
Julio <julio.junior02@gmail.com>
2021-03-30 20:33:38 -0500

    Add create playlists feature

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ db.create_all()
 
 @app.before_request
 def authenticate_before_req():
-    """Add user object flask global object if user token is in session"""
+    """Add user object flask global object if user token is in session."""
     
     if 'USER_ID' in session:
         g.user = User.query.get(session['USER_ID'])
@@ -26,36 +26,45 @@ def authenticate_before_req():
 
 @app.route('/', methods=['GET'])
 def show_home():
+    """Show home page."""
     # top_songs = requests.get(api)
 
     return render_template('home.html', isHome=True)
 
 @app.route('/search', methods=['GET'])
 def search_song():
+    """Search songs using term from GET request.
+    Return page without songs if no songs are found."""
+
     term = request.args['term']
+
     req = requests.get(api.SEARCH_URL, headers=api.API_HEADERS, params=dict(term=term))
     
     if req.status_code is not 200:
         return render_template('results.html', term=term)
 
     songs = req.json()['tracks']['hits']
-
     return render_template('results.html', songs=songs, term=term)
 
 @app.route('/songs/<int:song_key>', methods=['GET'])
 def show_song(song_key):
-    """Show song, along with song recomendations. 404 if no songs found."""
+    """Show song, along with song recomendations. 404 if song not found."""
+
     song = get_song(song_key)
+
+    if not song:
+        return abort(404)
+
     recom_req = requests.get(api.SONG_RECOMMENDATIONS_URL, headers=api.API_HEADERS, params=dict(key=song_key))
     recom_songs = recom_req.json()['tracks']
-
-    if not song and not recom_songs:
-        return abort(404)
 
     return render_template('song.html', song=song, songs=recom_songs)
 
 @app.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    """Show/handle signup, depending on request method.
+    Redirect if user is signed in, redirect same page if username is taken."""
+
     if g.user:
         flash('You have signed up already.', 'dark')
         return redirect('/')
@@ -105,7 +114,7 @@ def sign_out():
 @app.route('/favorite', methods=['POST'])
 def favorite():
     user = g.user
-    song_id = request.json['id']
+    song_id = request.json['key']
     song_fav = Favorite(user_id=user.id, song_key=song_id)
 
     user.favorites.append(song_fav)
@@ -127,20 +136,42 @@ def favorite():
 
 @app.route('/favorites')
 def show_favorites():
-
     if not g.user:
         flash('Sign up to favorite songs', 'light')
 
         return redirect('/sign-up')
-
+    
     songs_json = [get_song(fav) for fav in g.user.favorites_keys]
-    print(g.user.favorites_keys)
-
     return render_template('favorites.html', songs=songs_json)
+
+@app.route('/playlists', methods=['GET', 'POST'])
+def playlists():
+    user = g.user
+    
+    if not user:
+        flash('Sign up to make playlsits', 'light')
+    
+        return redirect('/sign-up')
+
+    form = PlaylistForm()
+
+    if form.validate_on_submit():
+        playlist = Playlist(user_id=user.id, name=form.name.data, description=form.description.data)
+        
+        user.playlists.append(playlist)
+        db.session.commit()
+        
+        return redirect('/playlists')
+
+    return render_template('playlists.html', form=form, playlists=user.playlists)
+
+@app.route('/playlists/<int:playlist_id>')
+def platlist(playlist_id):
+
+    return render_template('/playlist')
 
 @app.errorhandler(404)
 def show_not_found(err):
-    
     return render_template('404.html')
 
 def get_song(song_key):
