d6b2711f4bf13a273a2da47191d5a9688cb505d8
Julio <julio.junior02@gmail.com>
2021-03-30 20:38:41 -0500

    Change music preview ui

--- static/script.js
+++ static/script.js
@@ -1,29 +1,37 @@
-async function submitSongFav(id){
-    const res = await axios.post('/favorite', {id});
+// Do favorite song request
+async function submitSongFav(key){
+    const res = await axios.post('/favorite', {key});
 
     return res;
 }
 
+// Do signout request
 async function signOut(){
     const res = await axios.post('/sign-out')
-    return res
 }
 
-$('li #fav-song-btn').on('click', async function(evt){
-    const songId = $(evt.target).data('song-id');
-    const res = await submitSongFav(songId);
-    console.log(res)
+
+// Handle favorite button. Grab song_id from button
+// Send request and add or remove class
+$('li #fav-song-btn, #fav-song-btn').on('click', async function(evt){
+    const songKey = $(evt.target).data('song-key');
+    const res = await submitSongFav(songKey);
 
     if (res.data.action === 'favorited'){
-        $(evt.target).removeClass('far fa-star').addClass('fas fa-star');
+        $(evt.target).removeClass('far').addClass('fas');
     }else{
-        $(evt.target).removeClass('fas fa-star').addClass('far fa-star');
+        $(evt.target).removeClass('fas').addClass('far');
 
     }
 });
 
+// Handle signout link. Request signout and refresh page
 $('#sign-out-anchor').on('click', async function(evt){
     evt.preventDefault()
     await signOut()
     window.location.reload()
+});
+
+$(function() {
+    let as = audiojs.createAll();
 });
