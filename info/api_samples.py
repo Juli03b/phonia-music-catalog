c6f2cd7aa3cd2cedee330950882cb2b2b45360ff
Julio <julio.junior02@gmail.com>
2021-04-06 04:53:52 -0500

    Add not found image and sample JSON song

---
+++ info/api_samples.py
@@ -0,0 +1,227 @@
+false = False
+true = True
+
+SONG_JSON = {
+  "albumadamid": "1443829916", 
+  "artists": [
+    {
+      "adamid": "640286", 
+      "id": "376"
+    }
+  ], 
+  "genres": {
+    "primary": "R&B/Soul"
+  }, 
+  "hub": {
+    "actions": [
+      {
+        "id": "1443831010", 
+        "name": "apple", 
+        "type": "applemusicplay"
+      }, 
+      {
+        "name": "apple", 
+        "type": "uri", 
+        "uri": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview128/v4/9f/3a/df/9f3adfb0-7541-3860-96af-f2125c7c8f31/mzaf_324610463328254079.plus.aac.ep.m4a"
+      }
+    ], 
+    "displayname": "APPLE MUSIC", 
+    "explicit": false, 
+    "image": "https://images.shazam.com/static/icons/hub/ios/v5/applemusic_{scalefactor}.png", 
+    "options": [
+      {
+        "actions": [
+          {
+            "name": "hub:applemusic:deeplink", 
+            "type": "applemusicopen", 
+            "uri": "https://music.apple.com/us/album/untitled-how-does-it-feel/1443829916?i=1443831010&mttnagencyid=769459046716559743&mttnsiteid=125115&mttn3pid=a_custom_779816081798873874&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios"
+          }, 
+          {
+            "name": "hub:applemusic:deeplink", 
+            "type": "uri", 
+            "uri": "https://music.apple.com/us/album/untitled-how-does-it-feel/1443829916?i=1443831010&mttnagencyid=769459046716559743&mttnsiteid=125115&mttn3pid=a_custom_779816081798873874&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios"
+          }
+        ], 
+        "beacondata": {
+          "providername": "applemusic", 
+          "type": "open"
+        }, 
+        "caption": "OPEN", 
+        "colouroverflowimage": false, 
+        "image": "https://images.shazam.com/static/icons/hub/ios/v5/overflow-open-option_{scalefactor}.png", 
+        "listcaption": "Open in Apple Music", 
+        "overflowimage": "https://images.shazam.com/static/icons/hub/ios/v5/applemusic-overflow_{scalefactor}.png", 
+        "providername": "applemusic", 
+        "type": "open"
+      }, 
+      {
+        "caption": "BUY", 
+        "colouroverflowimage": false, 
+        "image": "https://images.shazam.com/static/icons/hub/ios/v5/itunes-overflow-buy_{scalefactor}.png", 
+        "listcaption": "Buy on iTunes", 
+        "overflowimage": "https://images.shazam.com/static/icons/hub/ios/v5/itunes-overflow-buy_{scalefactor}.png", 
+        "providername": "itunes", 
+        "type": "buy"
+      }
+    ], 
+    "providers": [
+      {
+        "actions": [
+          {
+            "name": "hub:spotify:androidsearchdeeplink", 
+            "type": "intent", 
+            "uri": "intent:#Intent;action=android.media.action.MEDIA_PLAY_FROM_SEARCH;component=com.spotify.music/.MainActivity;S.referrer=Shazam;S.query=Untitled%20%28How%20Does%20It%20Feel%29%20D%27Angelo;end"
+          }, 
+          {
+            "name": "hub:spotify:iossearchdeeplink", 
+            "type": "uri", 
+            "uri": "spotify:search:Untitled%20%28How%20Does%20It%20Feel%29%20D%27Angelo"
+          }
+        ], 
+        "caption": "Open in Spotify", 
+        "images": {
+          "default": "https://images.shazam.com/static/icons/hub/ios/v5/spotify_{scalefactor}.png", 
+          "overflow": "https://images.shazam.com/static/icons/hub/ios/v5/spotify-overflow_{scalefactor}.png"
+        }, 
+        "type": "SPOTIFY"
+      }, 
+      {
+        "actions": [
+          {
+            "name": "hub:deezer:searchdeeplink", 
+            "type": "uri", 
+            "uri": "deezer-query://www.deezer.com/play?query=%7Btrack%3A%27Untitled+%28How+Does+It+Feel%29%27%20artist%3A%27DAngelo%27%7D"
+          }
+        ], 
+        "caption": "Open in Deezer", 
+        "images": {
+          "default": "https://images.shazam.com/static/icons/hub/ios/v5/deezer_{scalefactor}.png", 
+          "overflow": "https://images.shazam.com/static/icons/hub/ios/v5/deezer-overflow_{scalefactor}.png"
+        }, 
+        "type": "DEEZER"
+      }
+    ], 
+    "type": "APPLEMUSIC"
+  }, 
+  "images": {
+    "background": "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/f0/48/e7/f048e778-17d6-9a45-04e2-4127bd2078ac/mza_7300933506351380236.png/800x800bb.jpeg", 
+    "coverart": "https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/b7/64/23/b7642343-5609-6efd-667d-d118f02ed1b9/00602557368925.rgb.jpg/400x400bb.jpeg", 
+    "coverarthq": "https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/b7/64/23/b7642343-5609-6efd-667d-d118f02ed1b9/00602557368925.rgb.jpg/800x800bb.jpeg", 
+    "joecolor": "b:000002p:fffeffs:d4d6d8t:cbcbccq:aaabad"
+  }, 
+  "isrc": "USVI29900212", 
+  "key": "257559", 
+  "layout": "5", 
+  "myshazam": {
+    "apple": {
+      "actions": [
+        {
+          "name": "myshazam:apple", 
+          "type": "uri", 
+          "uri": "https://music.apple.com/us/album/untitled-how-does-it-feel/1443829916?i=1443831010&mttnagencyid=769459046716559743&mttnsiteid=125115&mttn3pid=a_custom_779816081798873874&mttnsub1=Shazam_ios&mttnsub2=5348615A-616D-3235-3830-44754D6D5973&itscg=30201&app=music&itsct=Shazam_ios"
+        }
+      ]
+    }
+  }, 
+  "sections": [
+    {
+      "metadata": [
+        {
+          "text": "Voodoo", 
+          "title": "Album"
+        }, 
+        {
+          "text": "Virgin Catalog (V81)", 
+          "title": "Label"
+        }, 
+        {
+          "text": "2000", 
+          "title": "Released"
+        }
+      ], 
+      "metapages": [
+        {
+          "caption": "D'Angelo", 
+          "image": "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/f0/48/e7/f048e778-17d6-9a45-04e2-4127bd2078ac/mza_7300933506351380236.png/800x800bb.jpeg"
+        }, 
+        {
+          "caption": "Untitled (How Does It Feel)", 
+          "image": "https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/b7/64/23/b7642343-5609-6efd-667d-d118f02ed1b9/00602557368925.rgb.jpg/800x800bb.jpeg"
+        }
+      ], 
+      "tabname": "Song", 
+      "type": "SONG"
+    }, 
+    {
+      "tabname": "Video", 
+      "type": "VIDEO", 
+      "youtubeurl": {
+        "actions": [
+          {
+            "name": "video:youtube", 
+            "share": {
+              "avatar": "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/f0/48/e7/f048e778-17d6-9a45-04e2-4127bd2078ac/mza_7300933506351380236.png/800x800bb.jpeg", 
+              "href": "https://www.shazam.com/track/257559/untitled-how-does-it-feel", 
+              "html": "https://www.shazam.com/snippets/email-share/257559?lang=-&country=US", 
+              "image": "https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/b7/64/23/b7642343-5609-6efd-667d-d118f02ed1b9/00602557368925.rgb.jpg/400x400bb.jpeg", 
+              "snapchat": "https://www.shazam.com/partner/sc/track/257559", 
+              "subject": "Untitled (How Does It Feel) - D'Angelo", 
+              "text": "I used Shazam to discover Untitled (How Does It Feel) by D'Angelo.", 
+              "twitter": "I used @Shazam to discover Untitled (How Does It Feel) by D'Angelo."
+            }, 
+            "type": "webview", 
+            "uri": "https://youtu.be/SxVNOnPyvIU?autoplay=1"
+          }
+        ], 
+        "caption": "D'Angelo - Untitled (How Does It Feel) (Official Music Video)", 
+        "image": {
+          "dimensions": {
+            "height": 720, 
+            "width": 1280
+          }, 
+          "url": "https://i.ytimg.com/vi/SxVNOnPyvIU/maxresdefault.jpg"
+        }
+      }
+    }, 
+    {
+      "actions": [
+        {
+          "id": "376", 
+          "type": "artistposts"
+        }, 
+        {
+          "id": "376", 
+          "type": "artist"
+        }
+      ], 
+      "avatar": "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/f0/48/e7/f048e778-17d6-9a45-04e2-4127bd2078ac/mza_7300933506351380236.png/800x800bb.jpeg", 
+      "id": "376", 
+      "name": "D'Angelo", 
+      "tabname": "Artist", 
+      "type": "ARTIST", 
+      "verified": false
+    }, 
+    {
+      "tabname": "Related", 
+      "type": "RELATED"
+    }
+  ], 
+  "share": {
+    "avatar": "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/f0/48/e7/f048e778-17d6-9a45-04e2-4127bd2078ac/mza_7300933506351380236.png/800x800bb.jpeg", 
+    "href": "https://www.shazam.com/track/257559/untitled-how-does-it-feel", 
+    "html": "https://www.shazam.com/snippets/email-share/257559?lang=en-US&country=US", 
+    "image": "https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/b7/64/23/b7642343-5609-6efd-667d-d118f02ed1b9/00602557368925.rgb.jpg/400x400bb.jpeg", 
+    "snapchat": "https://www.shazam.com/partner/sc/track/257559", 
+    "subject": "Untitled (How Does It Feel) - D'Angelo", 
+    "text": "I used Shazam to discover Untitled (How Does It Feel) by D'Angelo.", 
+    "twitter": "I used @Shazam to discover Untitled (How Does It Feel) by D'Angelo."
+  }, 
+  "subtitle": "D'Angelo", 
+  "title": "Untitled (How Does It Feel)", 
+  "type": "MUSIC", 
+  "url": "https://www.shazam.com/track/257559/untitled-how-does-it-feel", 
+  "urlparams": {
+    "{trackartist}": "D%27Angelo", 
+    "{tracktitle}": "Untitled+%28How+Does+It+Feel%29"
+  }
+}
